5d8e6925fe9981e50082617786259c8d44a107e9
Nguyễn Ngọc Thiện <162625134+thienitcoder2004@users.noreply.github.com>
2026-03-19 14:31:11 +0700

    chore: init project with frontend and backend

---
+++ backend/src/test/java/com/bomberserver/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bomberserver.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
